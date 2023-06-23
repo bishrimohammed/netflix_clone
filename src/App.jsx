284df9aebb9c6ca5372d88eb7747056f3ed663bc
bishri mohammed <bishri60@gmail.com>
2023-06-23 21:41:52 +0300

    commit 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Route, Routes, useRoutes } from "react-router-dom";
+
+import Home from "./pages/Home";
 import Header from "./components/Header";
-import Features from "./components/Features";
-import Footer from "./components/Footer";
-import Faq from "./components/Faq";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState(null);
 
   return (
     <>
-      <Header />
-      <Features />
-      <Faq/>
-      <Footer />
+      <Home />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { BrowserRouter } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
